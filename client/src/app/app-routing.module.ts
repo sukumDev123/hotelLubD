c902cd5bdf68bbeae6ba4a50309c9e0f362159f3
sukumHomeComputer <kungo2009@hotmail.com>
2018-06-06 15:54:10 +0700

    core page home page 404 page 500 page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminCoreComponent } from './components/admin/admin-core/admin-core.component';
+
+import { SiginupComponent } from './components/auth/siginup/siginup.component';
 import { SignupAdminComponent } from './components/admin/signup-admin/signup-admin.component';
+import { ContentAdminComponent } from './components/admin/content-admin/content-admin.component';
+import { ContentPriceComponent } from './components/admin/content-price/content-price.component';
+import { AdminCoreComponent } from './components/admin/admin-core/admin-core.component';
+import { Page404Component } from './components/page404/page404.component';
+import { Page500Component } from './components/page500/page500.component';
+import { CoreComponent } from './components/core/core.component';
+import { HeaderComponent } from './components/header/header.component';
 
 const routes: Routes = [
   {path: 'admin' , component: AdminCoreComponent , children : [
     { path : 'signup' , component : SignupAdminComponent}
-  ]}
+  ]},
+
+  { path : 'not/found' , component : Page404Component },  
+    
+  { path: '', redirectTo: '/core/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/not/found', pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -14,3 +27,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const AppComponentRoute = [ SiginupComponent,SignupAdminComponent  , ContentAdminComponent,ContentPriceComponent,AdminCoreComponent,Page404Component , Page500Component , CoreComponent , HeaderComponent];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , AppComponentRoute } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SiginupComponent } from './components/auth/siginup/siginup.component';
-import { SignupAdminComponent } from './components/admin/signup-admin/signup-admin.component';
-import { ContentAdminComponent } from './components/admin/content-admin/content-admin.component';
-import { ContentPriceComponent } from './components/admin/content-price/content-price.component';
-import { AdminCoreComponent } from './components/admin/admin-core/admin-core.component';
 import { AdminServiceService } from './services/admin/inforS/admin-service.service';
+import { HomeComponent } from './components/home/home.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SiginupComponent,
-    SignupAdminComponent,
-    ContentAdminComponent,
-    ContentPriceComponent,
-    AdminCoreComponent
+    AppComponentRoute,
+    HomeComponent,
+
+
   ],
   imports: [
     BrowserModule,
