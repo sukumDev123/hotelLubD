d96e13814846f2bbc06f4bb8b7b0675ef965880f
sukumDevNotebook <s59122201052@ssru.ac.th>
2018-06-08 13:07:10 +0700

    Home Page

--- client/src/app/components/core/core.component.ts
+++ client/src/app/components/core/core.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-core',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./core.component.css']
 })
 export class CoreComponent implements OnInit {
-
+  @ViewChild("headerCore") headerCoreSave: ElementRef;
   constructor() { }
 
+
+
   ngOnInit() {
+    $("#navBar_box ul li a").css({
+      "color": "rgb(0,0,0)",
+      "font-weight": "bold",
+
+    })
+    $("#navBar_box").css({
+      "color":"black"
+    })
+    $(window).scroll(function () {
+
+      if ($(document).scrollTop() > 100) {
+        $("#navBar_box").css({
+          "position": "fixed",
+          "width": "100%",
+          "z-index": "105",
+          "background": "rgba(255,255,255,0.8)"
+        })
+        $("#navBar_box ul li a").css({
+          "color": "rgb(0,0,0)",
+        })
+      } else {
+        $("#navBar_box ul li a").css({
+          "color": "rgba(0,0,0,1)",
+          "font-weight": "bold"
+        })
+        $("#navBar_box").css({
+          "position": "relative",
+          "background": "rgba(0,0,0,0)",
+        })
+      }
+
+    })
   }
+
 
 }
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { DataShowService } from '../../services/dataShow/data-show.service';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  data_info : any = {};
-  constructor(private _readData : DataShowService) { }
+  data_info: any = {};
+  constructor(private _readData: DataShowService) { }
 
   ngOnInit() {
     this._readData.readData().subscribe(suc => {
-      console.log(suc)
-      this.data_info  = suc.data;
+      this.data_info = suc.data;
     }, err => {
       console.log(err)
     })
+
+    
   }
 
 }
