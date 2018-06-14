478fa03b66bd4b149b9086c6a659913f7e6f6c58
sukumHomeComputer <kungo2009@hotmail.com>
2018-06-14 09:15:19 +0700

    update booking and remove booking success

--- server/modules/booking/controllers/booking_controller.js
+++ server/modules/booking/controllers/booking_controller.js
@@ -27,7 +27,7 @@ function updateAsync(data) {
         try {
             let userUpdate = await User.findByIdAndUpdate(data._id, data)
             let getUser = await User.findById(data._id).select("displayname _id email phone reserveNum")
-           
+
             res(getUser)
         } catch (error) {
             rej(error)
@@ -40,7 +40,7 @@ function reserveNowFunc(data) {
         let booking = new Booking(data.body)
         booking.checkIn = new Date(booking.checkIn)
         booking.checkOut = new Date(booking.checkOut)
-        
+
         booking.save(err => {
             if (err) {
                 rej(err)
@@ -56,13 +56,31 @@ function reserveNowFunc(data) {
 
 /*********** */
 
-export  function showA_reser(req,res) {
-    if(req.bookingParam){
+export function showA_reser(req, res) {
+    if (req.bookingParam) {
         res.json(req.bookingParam)
     }
 }
-export async function updateReserveRoom(req, res) {}
-export async function deleteReserveRoom(req, res) {}
+export async function updateReserveRoom(req, res) {
+    try {
+        let updateBooking = await Booking.findByIdAndUpdate(req.bookingParam._id , req.body)
+        let findUser = await Booking.findById(req.bookingParam._id)
+        res.json(findUser)
+    } catch (error) {
+        res.status(403).json(error)
+    }
+}
+export async function deleteReserveRoom(req, res) {
+    try {
+        let removeBooking = await Booking.findByIdAndRemove(req.bookingParam._id)
+        res.json({
+            message : "Delete list booking success."
+        })
+    } catch (error) {
+        res.status(403).json(error)
+        
+    }
+}
 export async function reserveRoom(req, res) {
 
     try {
@@ -79,13 +97,15 @@ export async function reserveRoom(req, res) {
     } catch (error) {
 
         console.log(error)
-       // res.status(error.status).json(error)
+        // res.status(error.status).json(error)
 
     }
 }
 export async function historyRoom(req, res) {
     try {
-        let bookings = await Booking.find({ "userBooking._id" : req.user._id}).sort('-create_at')
+        let bookings = await Booking.find({
+            "userBooking._id": req.user._id
+        }).sort('-create_at')
         res.json(bookings)
     } catch (error) {
         console.log(error)
@@ -95,10 +115,10 @@ export async function getParamRoom(req, res, next, paramReser) {
 
     try {
         let bookParam = await Booking.findById(paramReser)
-        if(bookParam){
+        if (bookParam) {
             req.bookingParam = bookParam
         }
-        next()        
+        next()
     } catch (error) {
         console.log(error)
     }
