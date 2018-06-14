93f82a33d2e383c1d39366205a5c9c696d759775
sukumHomeComputer <kungo2009@hotmail.com>
2018-06-14 10:02:40 +0700

    #update content -> routes

--- server/modules/content/routes/content_route.js
+++ server/modules/content/routes/content_route.js
@@ -6,7 +6,9 @@ export function contentRoutes(){
     const router = express.Router()
 
     router.post('/add' ,checkUserLogin , cc.addContent)
-    
+    router.get('/contents',cc.getContents)
+    router.route('/content/:idContent').get(cc.getContent).put(cc.updateContent).delete(cc.removeContent)
+    router.param('idContent' , cc.getParamContent)
 
     return router
 }
