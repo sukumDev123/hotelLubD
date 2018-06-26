74d619cb9084a4428ffa5ff3ec380bd1127ee667
sukumHomeComputer <kungo2009@hotmail.com>
2018-06-26 15:30:24 +0700

    #{data add chaeh}

--- client/src/app/components/admin/data-add-home/data-add-home.component.ts
+++ client/src/app/components/admin/data-add-home/data-add-home.component.ts
@@ -3,25 +3,31 @@ import { UserGlobalService } from '../../../services/users/user/user-global.serv
 import { DataShowService } from '../../../services/dataShow/data-show.service';
 import { DataResort } from '../../../interface/data.interface';
 
+class DataResortClass {
+  private _data : DataResort
+  constructor(data : DataResort ) {
+    this._data = data
+  }
+  check_Empty() {
+    /**
+     * For Check Value on _data need to not empty
+     */
+  }
+}
+
 @Component({
   selector: 'app-data-add-home',
   templateUrl: './data-add-home.component.html',
   styleUrls: ['./data-add-home.component.css']
 })
 export class DataAddHomeComponent implements OnInit {
-  private _data_ : DataResort
+  private _data_ 
   constructor(private _user : UserGlobalService , private _dataResort : DataShowService) { }
 
   ngOnInit() {
     this._dataResort.readData().subscribe(suc => {
-      this._data_ = suc
+      this._data_ = new DataResortClass(suc)
     })
   }
-  dataShow() : DataResort {
-    return this._data_
-  }
-  dataChange()  {
-
-  }
-
+  
 }
