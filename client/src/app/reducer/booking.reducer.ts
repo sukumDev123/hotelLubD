5d155c24f5221a63ee9017f0c1be91638f238b47
sukumDev123 <kungo2009@hotmail.com>
2018-07-10 18:54:36 +0700

    #Booking created to conponents and register to reducer on ngrx

--- client/src/app/reducer/booking.reducer.ts
+++ client/src/app/reducer/booking.reducer.ts
@@ -2,74 +2,23 @@ import {
   ActionReducer,
   Action
 } from '@ngrx/store'
+import {
+  Booking
+} from '../interface/booking.interface';
 
 
+export const ADDROOMS = "ADDROOMS"
+export const DELETEROOM = "DELETEROOM"
+export const UPDATEROOM = "UPDATEROOM"
 
-interface Book {
-  name: string,
-    number: number,
-    priceRoom: number,
-    liveDate: Date,
-    liveLatest: Date
-}
-export interface Booking {
-  room: Array < Book > ,
-    state: boolean,
-    checkIn: Date,
-    checkOut: Date,
-    totalPrice: number,
-    userBooking: {}
-}
-export const initialState: Booking = {
-  room: [{
-      name: "01",
-      number: 1,
-      priceRoom: 400,
-      liveDate: new Date(),
-      liveLatest: new Date()
-    },
-    {
-      name: "02",
-      number: 2,
-      priceRoom: 400,
-      liveDate: new Date(),
-      liveLatest: new Date()
-    }
-  ],
-  state: false,
-  checkIn: new Date(),
-  checkOut: new Date(),
-  totalPrice: 0,
-  userBooking: {}
-}
 
-const bookingReducer = (state = initialState, action: Action) => {
+const bookingReducer = (state: Booking, action: Action) => {
   switch (action.type) {
-    case "SHOWBOOKING":
-      {
-        return {
-          ...state
-        }
-      }
-    case "ADD":
-      {
-        let total = 0
-        state.room.forEach(suc => {
-          total = total + suc.priceRoom
-        })
-        return {
-          ...state,
-          room: [
-            ...state.room
-          ],
-          totalPrice: total
-        }
-      }
-    default:
-      {
 
-        return state
-      }
+    default: {
+
+      return state 
+    }
   }
 
 
